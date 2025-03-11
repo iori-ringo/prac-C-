148bd62e3d624aa87ba6a20a82a1ec3b771785af
iori <iori1001k@gmail.com>
2025-03-11 18:09:21 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("ループ開始");
+    int sum = 0;
+for (int i = 0; i < 10; i++)
+{
+    sum += i;
+    Console.WriteLine(i);
+}
+Console.WriteLine("ループの終了");
+Console.WriteLine("合計値 :"  +sum);
